1f31385f747d965d97076ae721cc73290c089ff5
shivmtripathi <41698028+shivmtripathi@users.noreply.github.com>
2018-08-18 23:58:37 +0530

    js function to find lcm

---
+++ javascript/DAY-14/lcm.js
@@ -0,0 +1,6 @@
+function lcm_two_numbers(x, y) {
+   if ((typeof x !== 'number') || (typeof y !== 'number')) 
+    return false;
+  return (!x || !y) ? 0 : Math.abs((x * y) / gcd_two_numbers(x, y));
+}
+console.log(lcm_two_numbers(3,15));
