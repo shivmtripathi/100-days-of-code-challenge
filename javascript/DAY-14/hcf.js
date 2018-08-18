f1ec282bd284499a75b96d6b31699a541b8c1b94
shivmtripathi <41698028+shivmtripathi@users.noreply.github.com>
2018-08-18 23:59:51 +0530

    js function to find gcf

---
+++ javascript/DAY-14/hcf.js
@@ -0,0 +1,11 @@
+function gcd_two_numbers(x, y) {
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}
+console.log(lcm_two_numbers(10,15));
