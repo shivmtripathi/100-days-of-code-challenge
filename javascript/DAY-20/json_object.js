0de4a0ff87e75795937a50325740018e44b8c454
shivmtripathi <41698028+shivmtripathi@users.noreply.github.com>
2018-09-05 00:23:47 +0530

    access json object

---
+++ javascript/DAY-20/json_object.js
@@ -0,0 +1,17 @@
+var myObj = {
+  "name":"KIET geoup of institutions",
+  "add":"Ghaziabad",
+  "departments": {
+    "department1":"MCA",
+    "department2":"MBA",
+   "department3":"B.tech",
+   "Btech":{
+       "branch1":"EC",
+       "branch2":"CS",
+       "branch3":"MI",
+   }
+   }
+}
+  
+console.log(myObj.departments.Btech.branch1);
+
