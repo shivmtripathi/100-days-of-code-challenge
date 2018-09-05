a863e9b538848b67c9e5b3fa5d637df941b7ad2e
shivmtripathi <41698028+shivmtripathi@users.noreply.github.com>
2018-09-06 00:48:02 +0530

    json stringify mehod

---
+++ javascript/DAY-20/json_stringify.js
@@ -0,0 +1,8 @@
+var obj = { 
+    name: "shivam",
+     age: 21, 
+     city: "Kanpur",
+     qualification:"postgraduate",
+         }
+var myJSON = JSON.stringify(obj);
+console.log(obj.qualification);
