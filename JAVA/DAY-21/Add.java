82fff8b106c9b502e13293e84e4cb0a5240b632b
shivmtripathi <41698028+shivmtripathi@users.noreply.github.com>
2018-09-06 00:12:45 +0530

    add two no with user input

--- JAVA/DAY-21/Add.java
+++ JAVA/DAY-21/Add.java
@@ -1,1 +1,18 @@
 
+public class Add{
+    public static void main(String[] args) {
+     
+        Scanner readme = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("enter the two no ");
+       a = readme.nextInt();
+       b= readme.nextInt();
+        c=a+b;
+        
+        System.out.println("addition of two no is\t"+c);
+
+        
+    }
+
+}
+    
