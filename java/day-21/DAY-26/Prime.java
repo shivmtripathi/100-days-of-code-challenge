624358a20a1ae444b07a5d32ed6014a87541b143
shivmtripathi <41698028+shivmtripathi@users.noreply.github.com>
2018-09-17 23:39:23 +0530

    java program to find a prime number

---
+++ java/day-21/DAY-26/Prime.java
@@ -0,0 +1,18 @@
+mport java.util.Scanner;
+ public class Prime {
+ public static void main(String[] args){
+ int n,i,c=0;
+ Scanner input = new Scanner(System.in);
+ System.out.println("enter number u want to chek");
+ n=input.nextInt();
+ for(i=2;i<=(n/2);++i){
+	if(n%i==0)
+	c=c+1;
+	}
+ if(c==0)
+  System.out.println("number is prime");
+
+ else if(c>0)
+   System.out.print("number is not prime");
+ }
+}
